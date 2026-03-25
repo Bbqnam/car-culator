4279fe18aaa211e411408761ed8607dadd2c19a7
Bbqnam <nguyen_phuong_nam@hotmail.com>
2026-03-25 10:26:27 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,7 +47,7 @@ const Index = () => {
         <img
           src="/logo.png"
           alt="Carculator logo"
-          className="h-14 w-auto object-contain"
+          className="h-12 w-auto object-contain"
         />
 
         <div className="flex items-baseline gap-2">
